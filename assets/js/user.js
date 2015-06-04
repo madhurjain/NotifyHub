7b896f627eca4ffd9be8e80cf9896a410e2a642f
Madhur <mformadhur@gmail.com>
2015-06-05 02:32:01 +0530

    Displaying notifications on phone
    - Added style.css to assets
    - Added animated drawer to display notifications on phone

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,11 +1,28 @@
 $(function() {
   if (window["WebSocket"]) {
+  
+    $iosDrawer = $('#ios>.drawer');
+    $androidDrawer = $('#android>.drawer');
+    
     var wsHost = "ws://" + window.location.host + "/ws/" + uid;
     conn = new WebSocket(wsHost);
     conn.onclose = function(evt) {
       console.log("Socket connection closed");
     }
     conn.onmessage = function(evt) {
+      json = jQuery.parseJSON(evt.data);
+      // APN sent if token is set
+      if(json['token']) {
+        $('p', $iosDrawer).text(json['alert']);
+        $iosDrawer.slideDown();
+        setTimeout(function() { $iosDrawer.slideUp() }, 4000);
+      }
+      // GCM sent if registration_id is set
+      if(json['registration_id']) {      
+        $('p', $androidDrawer).text(json['title']);
+        $androidDrawer.slideDown();
+        setTimeout(function() { $androidDrawer.slideUp() }, 4000);
+      }
       console.log(evt.data);
     }
   }
